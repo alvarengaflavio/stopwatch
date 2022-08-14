cc7b7aa984993837c79c1370e771ad527c14f570
alvalenda <banysan@msn.com>
2022-08-14 10:49:27 -0300

    settimer functions +wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,14 @@ const main = () => {
   const startButton = document.querySelector("#startButton");
   const pauseButton = document.querySelector("#pauseButton");
   const resetButton = document.querySelector("#resetButton");
+  const setButton = document.querySelector("#setButton");
 
   const stopWatch = new StopWatch({
     start: startButton,
     pause: pauseButton,
     reset: resetButton,
     display: timeDisplay,
+    set: setButton,
   });
 
   stopWatch.setButtonEvents();
--- assets/js/stopwatch.class.js
+++ assets/js/stopwatch.class.js
@@ -1,5 +1,5 @@
 class StopWatch {
-  constructor({ start, pause, reset, display }) {
+  constructor({ start, pause, reset, display, set }) {
     this.start = 0;
     this.epasedTime = 0;
     this.currentTime = 0;
@@ -12,23 +12,47 @@ class StopWatch {
     this.startButton = start;
     this.pauseButton = pause;
     this.resetButton = reset;
+    this.setButton = set;
     this.timeDisplay = display;
+    this.time = { hours: 0, minutes: 0, seconds: 0 };
+    this.countdown = { totalSeconds: 0, seconds: 0 };
   }
 
-  updateTime() {
+  updateTime(countdown = false) {
     this.currentTime = Date.now();
     this.epasedTime = this.currentTime - this.start;
-    this.hours = Math.floor(this.epasedTime / 3600000);
-    this.minutes = Math.floor((this.epasedTime % 3600000) / 60000);
-    this.seconds = Math.floor((this.epasedTime % 60000) / 1000);
 
-    this.timeDisplay.textContent = `${pad(this.hours)}:${pad(
-      this.minutes
-    )}:${pad(this.seconds)}`;
+    if (!countdown) {
+      this.time.hours = Math.floor(this.epasedTime / 3600000);
+      this.time.minutes = Math.floor((this.epasedTime % 3600000) / 60000);
+      this.time.seconds = Math.floor((this.epasedTime % 60000) / 1000);
 
+      this.displayTime(this.time.hours, this.time.minutes, this.time.seconds);
+      return;
+    }
+
+    const seconds = Math.floor(this.epasedTime / 1000);
+    this.countdown.seconds = this.countdown.totalSeconds - seconds;
+    if (this.countdown.seconds < 0) {
+      this.soundAlarm();
+      this.resetButton.click();
+      return;
+    }
+    this.time.hours = Math.floor(this.countdown.seconds / 3600);
+    this.time.minutes = Math.floor((this.countdown.seconds % 3600) / 60);
+    this.time.seconds = Math.floor(this.countdown.seconds % 60);
+
+    this.displayTime(this.time.hours, this.time.minutes, this.time.seconds);
+  }
+
+  displayTime(hours, minutes, seconds) {
     function pad(number) {
       return number < 10 ? "0" + number : number;
     }
+
+    this.timeDisplay.textContent = `${pad(hours)}:${pad(minutes)}:${pad(
+      seconds
+    )}`;
   }
 
   startTime() {
@@ -57,10 +81,36 @@ class StopWatch {
     this.currentTime = 0;
     this.paused = true;
     this.isRunning = false;
-    this.hours = 0;
-    this.minutes = 0;
-    this.seconds = 0;
+    this.time.hours = 0;
+    this.time.minutes = 0;
+    this.time.seconds = 0;
     this.timeDisplay.textContent = "00:00:00";
+  }
+
+  setCountdown(hours, minutes, seconds) {
+    const countdown = {};
+    countdown.hours = hours;
+    countdown.minutes = minutes;
+    countdown.seconds = seconds;
+    this.time = { ...countdown };
+    this.countdown.totalSeconds = hours * 3600 + minutes * 60 + seconds;
+    this.countdown.seconds = this.countdown.totalSeconds;
+    console.log(this.countdown.seconds);
+    if (this.paused) {
+      this.paused = false;
+      this.start = Date.now();
+      this.interval = setInterval(() => {
+        this.updateTime(true);
+      }, 1000);
+      this.isRunning = true;
+    }
+  }
+
+  soundAlarm() {
+    const audio = new Audio(
+      "http://soundfxcenter.com/video-games/street-fighter/8d82b5_Street_Fighter_Hadouken_Sound_Effect.mp3"
+    );
+    audio.play();
   }
 
   setButtonEvents() {
@@ -72,6 +122,9 @@ class StopWatch {
     });
     this.resetButton.addEventListener("click", () => {
       this.resetTime();
+    });
+    this.setButton.addEventListener("click", () => {
+      this.setCountdown(0, 0, 10);
     });
   }
 }
